a7c56cccd1777804ef3bbb5102e24de51ec10660
Ripsa Niemi <ripsa.niemi@gmail.com>
2019-12-02 19:22:02 +0200

    week 5

--- data/create_human.R
+++ data/create_human.R
@@ -37,3 +37,56 @@ str(human)
 
 # Then let's create a file out of it
 write.csv(human, file = "data/human.csv", row.names = FALSE)
+
+###############################
+# WEEK 5, wrangling continues #
+###############################
+
+# reading the data
+
+human <- read.csv(file = "data/human.csv")
+str(human)
+dim(human)
+# data has 195 observations and 19 variables
+# Variables are for example HDI index and rank, GNI indekx, GII index and rank, country, life expectancy, education mean 
+# expectancy, maternity mortality, women's parlament represenment, women and mens education and labour ratios
+
+# 1) mutating GNI variable to numeric
+human$gni <- as.numeric(human$gni)
+
+# 2) excluding unwanted varibles, keeping ( "Country", "Edu2.FM", "Labo.FM", "Edu.Exp", 
+# "Life.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F")
+
+human_ <- dplyr::select(human, one_of("country", "eduRatio", "labourRatio", "exp_edu", "life_exp", 
+                                      "gni", "mater_mor", "ado_birth", "parl_rep"))
+
+# 3) Removing missing rows
+
+complete.cases(human_)
+
+h <- data.frame(human_[-1], comp = complete.cases(human_))
+
+# filter out all rows with NA values
+human__ <- filter(human_, h$comp == TRUE)
+
+# 4) remove regions
+
+tail(human__, n = 10)
+human__ <- slice(human__, 1:155)
+tail(human__)
+
+# 5) countries as rownames
+
+rownames(human__) <- human__$country
+
+human__ <- dplyr::select(human__, - country)
+
+# extra: changing colnames since I didnt like the ones I made up myself
+colnames(human__) <- c("Edu2.FM", "Labo.FM", "Edu.Exp", "Life.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F")
+
+str(human__)
+head(human__)
+
+summary(human__)
+
+write.table(human__, file = "data/human.csv", sep = ",", row.names = T, dec = ".")
